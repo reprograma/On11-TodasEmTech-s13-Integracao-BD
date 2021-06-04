7e5cfb1619c5b765362ceaf72ad6f9d1c06fa7d4
Karlla Nascimento <karllas.nascimento@gmail.com>
2021-06-03 22:05:59 -0300

    Atividades aula revisão

--- crudMongo/src/data/database.js
+++ crudMongo/src/data/database.js
@@ -1,6 +1,9 @@
-const mongoose = require("mongoose")
+require("dotenv").config()
 
-const connect = () => {mongoose.connect('mongodb://localhost:27017/livros', {
+const mongoose = require("mongoose")//importa o mongoose
+
+
+const connect = () => {mongoose.connect(process.env.DATABASE_URL, {
    useNewUrlParser: true,
    useUnifiedTopology: true
 }).then(console.log("Mongodb conectado")).catch(err => console.err)
--- crudMongo/src/routes/livros.routes.js
+++ crudMongo/src/routes/livros.routes.js
@@ -1,44 +1,21 @@
 const express = require("express")
 const router = express.Router()
+const controller = require("../controllers/livrosController")
 
 const Livro = require("../models/livro")
 
 // Create -> Post
-router.post("/", async (req, res)=>{
-   //crie um novo livro
-   const livro = new Livro({
-      nome: req.body.nome,
-      autora: req.body.autora,
-      paginas: req.body.paginas,
-      criadoEm: req.body.criadoEm
-   })
+router.post("/", controller.criarLivro)
 
-   //tentar mandar uma resposta e salvar no mongo
-   try {
-      const novoLivro = await livro.save() // aqui que está salvando no banco de dados
-      res.status(201).json(novoLivro) // enviando resposta do servidor
-   } catch (err){
-      res.status(400).json({message: err.message})
-   }
-})
+//Read/Ler -> Get
+router.get('/', controller.listaLivros)
 
-//Read -> Get
+router.get('/:id', controller.listaUmLivro)
 
-router.get("/", async (req, res)=>{
-   const livros = await Livro.find() //listando todos os livros
-   res.status(200).json(livros)
-})
+//Update/atualizar -> Patch
+router.patch('/:id', controller.atualizaLivro)
 
-//Update -> Patch
-
-router.patch("/", (req, res)=>{
-   
-})
-
-//Delete -> Delete
-
-router.delete("/", (req, res)=>{
-   
-})
+//Delete/deletar -> Delete
+router.delete('/:id', controller.deletaLivro)
 
 module.exports = router
